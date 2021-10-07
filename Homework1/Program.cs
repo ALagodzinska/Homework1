81da4977239a8b0b8109806dae36279fedbee738
ALagodzinska <anastasijalagodzinska@gmail.com>
2021-10-07 20:44:27 +0300

    Add project files.

---
+++ Homework1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Nice to meet you!");
+            Console.WriteLine("");
+            Console.WriteLine("My name is Anastasija and I'm learning to code.");
+            Console.WriteLine("In my free time I'm interested in fashion design, dancing and programming :)");
+            Console.WriteLine("Overall I'm adventurous person and I love to explore and learn new things.");
+            Console.WriteLine("");
+            Console.WriteLine("Thanks for your attention!");
+            Console.Write("Bye!");
+            Console.ReadLine();
+        }
+    }
+}
